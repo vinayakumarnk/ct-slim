2439454fde8f496c8e41f3a9437b7ea6c439130b
Vinayakumar <vinay4social@gmail.com>
2015-06-16 20:42:22 +0530

    db test
    db test

--- db_test.php
+++ db_test.php
@@ -3,7 +3,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$this->conn	=	new MySQLi('us-cdbr-iron-east-02.cleardb.net', 'b3839c81c0b0de', 'f32b049b', 'heroku_5139ce7a09c55ce');
+$conn	=	new MySQLi('us-cdbr-iron-east-02.cleardb.net', 'b3839c81c0b0de', 'f32b049b', 'heroku_5139ce7a09c55ce');
 
                 if(mysqli_connect_errno()){
                     echo 'Failed to connect to MySQL '.mysqli_connect_error();
