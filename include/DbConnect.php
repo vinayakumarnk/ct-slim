522ee863dbc9845f59c438157f6ce085549210f2
Vinayakumar <vinay4social@gmail.com>
2015-06-16 20:21:43 +0530

    debugging
    debugging

--- include/DbConnect.php
+++ include/DbConnect.php
@@ -15,12 +15,12 @@ class DbConnect{
 	* @return Database Connection handler
 	*/
 	function connect(){
-	
+            print_r('conf bfr');
 		require_once dirname(__FILE__).'/Config.php';
-		
+		print_r('conf aftr');
 		//connecting mysql database
 		$this->conn	=	new MySQLi(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
-                
+                print_r('aftr conn');
                 //check for database connection error
                 if(mysqli_connect_errno()){
                     echo 'Failed to connect to MySQL '.mysqli_connect_error();
--- include/config.php
+++ include/config.php
@@ -2,6 +2,8 @@
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+
+print_r('i m in confg');
 /**
  * Database configuration
  */
@@ -13,7 +15,7 @@ define('DB_USERNAME', 'b3839c81c0b0de');
 define('DB_PASSWORD', 'f32b049b');
 define('DB_HOST', 'us-cdbr-iron-east-02.cleardb.net');
 define('DB_NAME', 'heroku_5139ce7a09c55ce');
-
+print_r('end of config');
 define('USER_CREATED_SUCCESSFULLY', 0);
 define('USER_CREATE_FAILED', 1);
 define('USER_ALREADY_EXISTED', 2);
