60eaaa97eb94864b30ae2d1d9822a39939a1f3df
Vinayakumar <vinay4social@gmail.com>
2015-06-16 19:49:34 +0530

    changed db config
    changed db config

--- include/config.php
+++ include/config.php
@@ -2,10 +2,14 @@
 /**
  * Database configuration
  */
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'vinay_slim');
+//define('DB_USERNAME', 'root');
+//define('DB_PASSWORD', '');
+//define('DB_HOST', 'localhost');
+//define('DB_NAME', 'vinay_slim');
+define('DB_USERNAME', 'b3839c81c0b0de');
+define('DB_PASSWORD', 'f32b049b');
+define('DB_HOST', 'us-cdbr-iron-east-02.cleardb.net');
+define('DB_NAME', 'heroku_5139ce7a09c55ce');
 
 define('USER_CREATED_SUCCESSFULLY', 0);
 define('USER_CREATE_FAILED', 1);
