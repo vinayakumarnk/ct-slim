4960b3a132b9c2f3f0e59f199357fe8330652f4b
Vinayakumar <vinay4social@gmail.com>
2015-06-17 12:10:57 +0530

    cr removed

--- include/Config.php
+++ include/Config.php
@@ -12,20 +12,6 @@ ini_set('display_errors', 1);
 //define('DB_PASSWORD', '');
 //define('DB_HOST', 'localhost');
 //define('DB_NAME', 'vinay_slim');
-//heroku
-define('DB_USERNAME', 'b3839c81c0b0de');
-define('DB_PASSWORD', 'f32b049b');
-define('DB_HOST', 'us-cdbr-iron-east-02.cleardb.net');
-define('DB_NAME', 'heroku_5139ce7a09c55ce');
 
-//ct
-//define('DB_USERNAME', 'cumulati_slim');
-//define('DB_PASSWORD', '123slim$%^');
-//define('DB_HOST', 'localhost');
-//define('DB_NAME', 'cumulati_slim');
-
-define('USER_CREATED_SUCCESSFULLY', 0);
-define('USER_CREATE_FAILED', 1);
-define('USER_ALREADY_EXISTED', 2);
 
 ?>
